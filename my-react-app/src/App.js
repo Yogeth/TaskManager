93bff3de25ee320f253b9607e60e9d3df8c43ced
Yogeth <yogethcr@gmail.com>
2025-09-21 10:23:59 +0530

    update App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Yogeth React
+          Yogeth shajsjakakwk
         </a>
       </header>
     </div>
